8a8f3e1932b9700525b537c71554c481ef8d7107
Dorian Maspataud <dorian@numeral.io>
2024-11-29 10:00:55 -0500

    Add latest changes.

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -1,3 +1,4 @@
+-- If you get stuck Generating an answer, try this fix first : https://github.com/yetone/avante.nvim/issues/612#issuecomment-2375729928
 return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
@@ -8,27 +9,14 @@ return {
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make BUILD_FROM_SOURCE=true',
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    'nvim-treesitter/nvim-treesitter',
     'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
-    {
-      -- support for image pasting
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
+    --- The below dependencies are optional,
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
     {
       -- Make sure to set this up properly if you have lazy=true
       'MeanderingProgrammer/render-markdown.nvim',
